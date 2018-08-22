d7b0971d97166daa3cf0d7429aa53a7f8b312866
RituRaj Singh <RituRaj Singh@192.168.0.100>
2018-08-23 04:18:08 +0700

    First Commit

---
+++ TestRecast1/src/main/java/com/contura/demo/TestRecast1Application.java
@@ -0,0 +1,12 @@
+package com.contura.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRecast1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRecast1Application.class, args);
+	}
+}
